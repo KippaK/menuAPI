2398597c0b37c1f0e765ce8bc3841e1136b1cc9a
KippaK <antti.venetjoki@gmail.com>
2023-05-16 12:16:27 +0300

    Changed file name to better describe file content and some other changes

--- example.cpp
+++ example.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <conio.h>
+#include <cstdlib>
 #include "menuapi.h"
+
+#if defined(_WIN32)
+#define WIN32_LEAN_AND_MEAN
+#define VC_EXTRALEAN
+#include <Windows.h>
+const HWND consoleWindow = GetConsoleWindow();
+#elif defined(__linux__)
+#include <sys/ioctl.h>
+#endif
 
 #define KEY_UP 72
 #define KEY_DOWN 80
@@ -18,6 +28,15 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+void closeWindow() {
+    #if defined(_WIN32)
+        if (consoleWindow != NULL) {
+            PostMessage(consoleWindow, WM_CLOSE, 0, 0);
+        }
+    #elif defined(__linux__)
+        std::system("killall -9 $(basename $SHELL)");
+    #endif
+}
 
 int main() {
     const Flags GLOBAL_FLAGS {STRECH_X, STRECH_Y, CENTER_X, CENTER_Y};
@@ -39,4 +58,7 @@ int main() {
     menu->start();
     cout << "Selected option: " << menu->getValueName() << endl;
     cout << "Index of selected option: " << menu->getValueIdx() << endl;
+    cout << "Press any key to exit";
+    _getch();
+    closeWindow();
 }
